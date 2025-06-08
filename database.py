fab0090b4279614b4ca3ab858068ce3cf5f0c3e9
RiptideReef <jditzel02@gmail.com>
2025-06-08 16:42:57 -0400

    Listing seasons to discord. Started with setting active season.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 import logging
 
 logger = logging.getLogger("database_logger")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 file_handler = logging.FileHandler("database.log")
 formatter = logging.Formatter(
@@ -19,7 +19,9 @@ def log_update(cursor, positive_msg, negative_msg):
         logger.debug(negative_msg)
 
 def get_connection():
-    return sqlite3.connect('sharkvivor.db', check_same_thread=False)
+    conn = sqlite3.connect('sharkvivor.db', check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 def setup_tables():
     conn = get_connection()
@@ -32,6 +34,16 @@ def setup_tables():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             discord_id TEXT UNIQUE NOT NULL,
             username TEXT
+        );
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS seasons (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            number INT UNIQUE NOT NULL,
+            episode INT DEFAULT 0,
+            status TEXT NOT NULL DEFAULT 'inactive'
         );
     ''')
 
@@ -55,8 +67,48 @@ def add_user(discord_id: str, username: str):
 
     log_update(c, 
                f"'users' table updated: discord_id={discord_id}, username={username}.", 
-               f"users table unchanged: discord_id={discord_id}, username={username}")
+               f"'users' table unchanged: discord_id={discord_id}, username={username}")
     
     conn.commit()
     conn.close()
 
+def add_season(season_name: str, season_number: int):
+    conn = get_connection()
+    c = conn.cursor()
+
+    command = '''
+        INSERT INTO seasons (name, number)
+        VALUES (?, ?)
+        ON CONFLICT(number)
+        DO UPDATE SET name = excluded.name
+        WHERE seasons.name IS NOT excluded.name;
+    '''
+    c.execute(command, (season_name, season_number))
+
+    log_update(c, 
+               f"'seasons' table updated: number={season_number}, name={season_name}.", 
+               f"'seasons' table unchanged: number={season_number}, name={season_name}")
+    
+    conn.commit()
+    conn.close()
+
+def get_all_seasons():
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM seasons ORDER BY number')
+    rows = c.fetchall()
+
+    conn.close()
+    return [dict(row) for row in rows]
+
+def activate_season(season_name: str, season_number: int) -> bool:
+    conn = get_connection()
+    c = conn.cursor()
+
+    command = '''
+        SELECT id, status FROM seasons
+        WHERE name = ? AND number = ?;
+    '''
+    c.execute(command, (season_name, season_number))
+    result = c.fetchone()
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 
 import database
 
-# Setup
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 guild_id = int(os.getenv('TEST_GUILD'))
@@ -23,10 +22,9 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    guild = discord.Object(id=guild_id)
     try:
-        synced = await bot.tree.sync(guild=guild)
-        print(f"Synced {len(synced)} command(s) to guild {guild.id}")
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s).")
     except Exception as e:
         print(f"Sync failed: {e}")
         return
@@ -44,18 +42,45 @@ def add_all_users():
 
 @bot.event
 async def on_member_join(member):
-    database.add_user(int(member.id), str(member.name))
+    database.add_user(str(member.id), str(member.name))
 
     role = discord.utils.get(member.guild.roles, name="Viewer")
     if role:
         await member.add_roles(role)
-        print(f"Assigned 'Viewer' role to {member.name}")
     else:
         print(f"Role 'Viewer' not found.")
 
 @bot.tree.command(name="hello", description="Say hello to the user.")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hello {interaction.user.name}!", ephemeral=True)
+    await interaction.response.send_message(f"Hello {interaction.user.name}!")
 
+@bot.tree.command(name="addseason", description="Register a new season to the database.")
+async def addseason(interaction: discord.Interaction, name: str, number: int):
+    database.add_season(name.strip(), number)
+    await interaction.response.send_message(f"Season **{name} (#{number})** added to the database.")
+
+@bot.tree.command(name="listseasons", description="List all of the seasons in the database.")
+async def listseasons(interaction: discord.Interaction):
+    seasons = database.get_all_seasons()
+
+    if not seasons:
+        await interaction.response.send_message("No seasons found in the database")
+        return
+    
+    season_list = "**Seasons in the database:**\n"
+    for season in seasons:
+        season_number = season['number']
+        season_name = season['name'].strip()
+        season_status = season['status']
+        season_list += f"**{season_number}. {season_name}** (Status: {season_status})\n"
+
+    await interaction.response.send_message(season_list)
+
+async def autocomplete_season(interaction: discord.Interaction, current: str) -> list[app_commands.Choice[str]]:
+    seasons = database.get_all_seasons()
+
+@bot.tree.command(name="setseason", description="Set an existing season to active and deactivate all others.")
+async def setseason(interaction: discord.Interaction, season: str):
+    pass
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
